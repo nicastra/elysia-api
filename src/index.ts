089abf0a38c5ea064e35d487d347bad74b2466be
nicastra <nicola@persissolo.id>
2024-11-17 15:23:43 +0700

    fixing : deployment

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import { Elysia, error, t } from "elysia";
 import { swagger } from "@elysiajs/swagger";
 import { note } from "./note";
 
-const app = new Elysia().use(swagger()).use(note).listen(3000);
+const app = new Elysia()
+  .use(swagger())
+  .use(note)
+  .listen(process.env.PORT ?? 3020);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
